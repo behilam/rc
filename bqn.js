da0948274f8392c2cea49507acfcaf42bb82af3f
Behilam <praehil@gmail.com>
2023-03-06 01:46:33 -0500

    Fix BQN arg parsing

--- bqn.js
+++ bqn.js
@@ -1,6 +1,5 @@
 const bqn = require("./bqnVM.js");
-const args = process.argv.slice(2).join(" ");
+const args = process.argv.slice(2).join("");
 
 const res = bqn.compile(args);
-
 console.log(bqn.fmt(bqn.run(...res)));
